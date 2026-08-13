264fba31a29a69be79a22608a4656c3a590f23b9
weshab71-wq <weshab71@gmail.com>
2026-08-13 12:04:26 +0300

    Update build.sh to apply Portal 2 patches
    Added commands to apply patches for Portal 2 in build.gradle, strings.xml, and AndroidManifest.xml.

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 export ANDROID_HOME=/home/jusic/android-sdk
+
+# Apply Portal 2 patches (Package ID, App Name, and Launch Mode)
+find . -type f -name 'build.gradle' -exec sed -i 's/com.valvesoftware.source/com.valvesoftware.portal2/g' {} +
+find . -type f -name 'strings.xml' -exec sed -i 's/Source Engine/Portal 2/g' {} +
+find . -type f -name 'AndroidManifest.xml' -exec sed -i "s/launchMode='standard'/launchMode='singleTask'/g" {} +
+
 ant debug
